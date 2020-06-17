2a13be32dac910003feb9c1095fb37330c26abf0
Eric  Hagood <eric@Erics-MacBook-Pro.local>
2020-06-17 10:33:38 -0500

    Fixed for loop so each player is being given the exact same hand

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 let deckID = '';
 let playerArray = [{name: 'north', points: 0}, {name: 'east', points: 0}, {name: 'player', points: 0}, {name: 'west', points: 0}];
+////////////////////
+///On load function. Loads in API after page has fully loaded to avoid any possible appending erorrs
+////////////////////
 $(()=>{
+    ///Creates the deck and gets the unique deck ID to be used in the rest of the program
     function getDeck(){
         $.ajax({
             url: 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
@@ -16,17 +20,20 @@ $(()=>{
     getDeck();
     $('.draw').on('click', draw);
     });
+//////////////
+///This function draws 13 cards into each of the players hands.
+//////////////
 function draw(){
-    $.ajax({
-    url: 'https://deckofcardsapi.com/api/deck/' + deckID + '/draw/?count=13',
-    // type: 'GET',
-    }).then((drawnCards)=>{
-        for (let i = 0; i < playerArray.length; i++){
-            playerArray[i].cards = drawnCards.cards;
-            console.log(playerArray[i]);
+    for (let i = 0; i < playerArray.length; i++){
+        $.ajax({
+        url: 'https://deckofcardsapi.com/api/deck/' + deckID + '/draw/?count=13',
+        // type: 'GET',
+        }).then((drawnCards)=>{
+                playerArray[i].cards = drawnCards.cards;
+                console.log(playerArray[i]);
+            
+        }), (error) =>{
+            console.error(error);
         }
-        
-    }), (error) =>{
-        console.error(error);
     }
 }
